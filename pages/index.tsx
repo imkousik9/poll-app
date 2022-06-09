b8210e75cca8a13f431c85c84d9442e54dd10ccf
Kousik Manna <45976284+imkousik9@users.noreply.github.com>
2022-06-09 15:22:41 +0530

    working trpc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,13 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
 
-import { prisma } from '../lib/prisma';
 import { trpc } from '../lib/trpc';
 
 const Home: NextPage = (props: any) => {
-  const { data, isLoading } = trpc.useQuery(['hello']);
+  const { data, isLoading } = trpc.useQuery(['getAllQuestions']);
 
   if (isLoading || !data) return <div>Loading...</div>;
 
-  return <div>{data?.greeting}</div>;
-};
-
-export const getServerSideProps = async () => {
-  const questions = await prisma.pollQuestion.findMany({});
-  return {
-    props: {
-      questions: JSON.stringify(questions)
-    }
-  };
+  return <div>{data[0]?.question}</div>;
 };
 
 export default Home;
